26644c0a0a6198b102953fd7e91ce9199f649af6
Kazuki Taira <hagegalka@gmail.com>
2017-11-10 10:57:56 +0900

    なるほど

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app1 = express();
+app1.use(express.static(__dirname + '/public'));
+app1.listen(3011, () => console.log('listen 3011'));
+
+const app2 = express();
+app2.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'X-HOGEHOGE');
+	next();
+});
+app2.use((req, res) => {
+	console.log('app2-> ', req.method, req.header('x-hogehoge'));
+	res.send('OK');
+});
+app2.listen(3012, () => console.log('listen 3012'));
